ff69d80324632b6cbf0ec5e389ef8a6199990302
Igor Medeiros <igor.medeiros@gmail.com>
2022-06-19 23:11:31 -0300

    Initial commit with a controller to check API status (running or not)

---
+++ crm33/src/main/java/br/com/igormedeiros/Crm33Application.java
@@ -0,0 +1,13 @@
+package br.com.igormedeiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crm33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crm33Application.class, args);
+	}
+
+}
---
+++ crm33/src/test/java/br/com/igormedeiros/Crm33ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igormedeiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crm33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
